7e4060411da9845503ef2c8405f36daecf49abce
Josbek Kalinowski Quispe <josbekkalinowskiquispe@Josbeks-MacBook-Pro.local>
2025-02-09 14:23:38 -0500

    new route added

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://kpi-test-rest.onrender.com';
+// For test
+//const API_BASE_URL = 'https://kpi-test-rest.onrender.com';
+const API_BASE_URL = 'https://fk4fz0eh38.execute-api.us-east-1.amazonaws.com';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
